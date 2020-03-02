334fa399810be1df21c860f0a05446a1b94cb963
unknown <gustavharry@hotmail.com>
2020-03-02 16:36:14 +0100

    Tried to push correctly, some packages might be missing

--- somethingsCooking/src/app/app-routing.module.ts
+++ somethingsCooking/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
+import { DummyComponent } from './dummy/dummy.component'
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "Recipes", component: DummyComponent },
+  { path: "Menus", component: DummyComponent },
+  { path: "Shopping_List", component: DummyComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- somethingsCooking/src/app/app.module.ts
+++ somethingsCooking/src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatMenuModule} from '@angular/material/menu';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { DummyComponent } from './dummy/dummy.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DummyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatMenuModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
